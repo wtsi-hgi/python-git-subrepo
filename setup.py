c612d58643c943b4386fa1a0a5d2e0aed8adaba0
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-07-05 15:48:53 +0100

    Updates URL.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     version="1.0.2",
     packages=find_packages(exclude=["tests"]),
     install_requires=open("requirements.txt", "r").readlines(),
-    url="https://github.com/wtsi-hgi/python-git-subrepo-wrapper",
+    url="https://github.com/wtsi-hgi/python-git-subrepo",
     license="MIT",
     description="Provides access to `git subrepo` in Python",
     long_description=read_markdown("README.md")
